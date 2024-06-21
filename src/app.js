303eaaf9b64dc9bbb35dc9c9267a911e715cf314
RicardoClaro7 <167456148+RicardoClaro7@users.noreply.github.com>
2024-06-21 13:40:20 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -4,3 +4,29 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
+
+let who = ["The dog", "My grandma", "The mailman", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "my phone", "the car"];
+let when = [
+  "before the class",
+  "when I was sleeping",
+  "while I was exercising",
+  "during my lunch",
+  "while I was praying",
+];
+
+function random(arreglo) {
+  return arreglo[Math.floor(Math.random() * arreglo.length)];
+}
+
+window.onload = function() {
+  let randomWho = random(who);
+  let randomAction = random(action);
+  let randomWhat = random(what);
+  let randomWhen = random(when);
+
+  let excuse =
+    randomWho + " " + randomAction + " " + randomWhat + " " + randomWhen + ".";
+  document.getElementById("excuse").innerHTML = excuse;
+};
